ab951447d4d6c5290c1207933387cac346788ba7
Philip Cass <frymaster@127001.org>
2015-02-15 21:01:11 +0000

    Debug JSON query for name->UUID requests
    WARNING: Massively spammy. Do not use in production

--- src/main/java/de/diddiz/util/UUIDFetcher.java
+++ src/main/java/de/diddiz/util/UUIDFetcher.java
@@ -14,6 +14,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
+import static org.bukkit.Bukkit.getLogger;
+
 // Adapted from https://gist.github.com/evilmidget38/26d70114b834f71fb3b4
 
 public class UUIDFetcher {
@@ -38,6 +40,7 @@ public class UUIDFetcher {
 	}
 
 	private static void writeBody(HttpURLConnection connection, String body) throws Exception {
+	    getLogger().info(body);
 		OutputStream stream = connection.getOutputStream();
 		stream.write(body.getBytes());
 		stream.flush();
